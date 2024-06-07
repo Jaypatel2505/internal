752be5db27839274dbd32c3c79a94d1fdfc01d35
Jaypate2505 <joy.patel2505@gmail.com>
2024-06-07 12:41:31 +0530

    Today Changes

--- JAVA22/src/com/basic/PatternDemo.java
+++ JAVA22/src/com/basic/PatternDemo.java
@@ -27,9 +27,8 @@ package com.basic;
  * Incree/Decree : <= then ++
  * 				   >= then --
  * 
- * 0
- * 0-1
- * 0-1-2
+ * for k:
+ * - k completely depends on i
  */
 public class PatternDemo {
 public static void main(String[] args) {
@@ -134,6 +133,41 @@ public static void main(String[] args) {
 		}
 		System.out.println();
 	}
+	
+	/*
+	 *     1
+	 *    12
+	 *   123
+	 *  1234
+	 * 12345
+	 */
+	for (int i = 1; i <= 5; i++) {
+		for (int k = i; k <=5; k++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=i; j++) {
+			System.out.print(j);
+		}
+		System.out.println();
+	}
+
+	/*
+	 *     1
+	 *    1 2
+	 *   1 2 3
+	 *  1 2 3 4
+	 * 1 2 3 4 5
+	 */
+	for (int i = 1; i <= 5; i++) {
+		for (int k = i; k <=5; k++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=i; j++) {
+			System.out.print(" "+j);
+		}
+		System.out.println();
+	}
+	
 	
 }
 }
--- JAVA22/src/com/task/YoungestPerson.java
+++ JAVA22/src/com/task/YoungestPerson.java
@@ -2,18 +2,14 @@ package com.task;
 
 public class YoungestPerson {
 public static void main(String[] args) {
-	int ram = 10, shyam = 1, ajay = 2;
+	int ram = 30, shyam = 40, ajay = 20;
 	
-	if (ram<=shyam) {
-		if (ram<=ajay) {
-			System.out.println("Ram is young");
-		} else {
-			System.out.println("Ajay is young");
-		}
-	} else if(shyam<=ajay){
-		System.out.println("Shyam is young");
+	if(ram<=shyam&&ram<=ajay){
+		System.out.println("Ram is young");
+	}else if (shyam<=ajay&&shyam<=ram) {
+		System.out.println("Shyam is Young");
 	}else {
-		System.out.println("Ajay is Young");
+		System.out.println("Ajay is young");
 	}
 }
 }
